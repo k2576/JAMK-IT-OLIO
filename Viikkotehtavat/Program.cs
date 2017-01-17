56ae2ae7bbecf115feda90aa6841601272e4bf12
Tomas <tomaskul@outlook.com>
2017-01-17 17:09:47 +0200

    Tehtävä1 tehty

---
+++ Viikkotehtavat/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tehtava();
+        }
+
+        static void Tehtava()
+        {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana 
+            //(yksi, kaksi tai kolme). Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
+            Console.WriteLine("Anna 1,2 tai 3 ja tulostan sen tekstinä");
+            string retval = Console.ReadLine();
+            int luku = int.Parse(retval);
+            if (luku == 1)
+            {
+                retval = "yksi";
+            }
+            else if (luku == 2)
+            {
+                retval = "kaksi";
+            }
+            else if (luku == 3)
+            {
+                retval = "kolme";
+            }
+            else
+            {
+                retval = "joku muu luku";
+            }
+            //tulostus käyttäjälle 
+            Console.WriteLine(retval);
+        }
+    }
+}
